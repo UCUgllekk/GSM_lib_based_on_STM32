c946e39f4736eeaf6f445464a855904d25e41686
Iryna Denysova <106862256+Shnapa@users.noreply.github.com>
2024-12-04 14:54:45 +0200

    Create gui.c

---
+++ Test/Core/Src/gui.c
@@ -0,0 +1,56 @@
+...
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+//    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+//    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+//    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+//
+//    /** Initializes the RCC Oscillators according to the specified parameters
+//    * in the RCC_OscInitTypeDef structure.
+//    */
+//    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
+//    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+//    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+//    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+//    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+//    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+//    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+//    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+//    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+//        Error_Handler();
+//    }
+//
+//    /** Initializes the CPU, AHB and APB buses clocks
+//    */
+//    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+//                                | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+//    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+//    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+//    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+//    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+//
+//    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+//        Error_Handler();
+//    }
+//    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_I2C1;
+//    PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+//    PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL;
+//    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+//        Error_Handler();
+//    }
+}
+
+void Error_Handler(void) {
+    __disable_irq();
+    while (1) {
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line) {
+    // Print debug info if needed
+}
+#endif /* USE_FULL_ASSERT */
